823342a8ba2de0fe39eedcd9bf73e4661a9ef441
arxip@inbox.ru <Poiskeone03>
2019-07-11 13:37:04 +0300

    start working on app

--- main.py
+++ main.py
@@ -1,66 +1,109 @@
+
 # IMPORT REQUESTS
 import requests
-import time
 # IMPORT KIVY
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager 
+
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
-class WeatherApp(App):
-    # CALL WEATHER API AND RETURN JSON
-    def call_weather_api(self):
-        url_by_city = 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&units=metric&appid=55ce897ca94456cf36a07274c8c2bc4c'
 
-        # lat = None
-        # lon = None
-        # url_by_coordinates = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}?lon={lon}&appid=55ce897ca94456cf36a07274c8c2bc4c'
+class Dashboard(Screen):
+    def __init__ (self, **kwargs):
+        super ().__init__(**kwargs)
+        self.parse_dashboard()
 
-        json_data = requests.get(url_by_city).json()
+
+    def request(self, city):
+        # REQUEST URL 
+        request_url= 'https://api.openweathermap.org/data/2.5/weather?q={},ru&units=metric&appid=55ce897ca94456cf36a07274c8c2bc4c'.format(city)
+        # SEND REQUEST AND TRANSLATE RESPONSE INTO JSON
+        json_data = requests.get(request_url).json()
+        # RETURN JSON
         return json_data
 
-    def parse_weather_json(self):
-        json = self.call_weather_api()
-        
-        parsed_json = dict()
 
-        parsed_json['city'] = json['name']
-        parsed_json['max_temp'] = json['main']['temp']
-        parsed_json['min_temp'] = json['main']['temp_min']
-        parsed_json['overhead_main'] = json['weather'][0]['main']
-        parsed_json['overhead_description'] = json['weather'][0]['description']
-        parsed_json['wind_speed'] = json['wind']['speed']
-        parsed_json['sunrise'] = time.strftime('%H:%M:%S', time.gmtime(json['sys']['sunrise']))
-        parsed_json['sunset'] = time.strftime('%H:%M:%S', time.gmtime(json['sys']['sunset']))
-
-        return parsed_json
+    def find_city_weather(self, isinstance):
+        #self.json_data = self.request(self.city_input.text)
+        self.changer()
 
 
-    # PARSE WEATHER 
-    def parse_weather(self):
+    # PARSE DASHBOARD
+    def parse_dashboard(self):
         al = AnchorLayout()
-        bl = BoxLayout( orientation = 'vertical', size_hint = [None, None], size = [300, 200] )
+        bl = BoxLayout( orientation = 'vertical', size_hint = [None, None], size = [300, 100] )
 
-        json = self.parse_weather_json()
+        city_input_label = Label(text='Введите название города: ')
+        self.city_input = TextInput(text='')
+        city_button = Button(text='Найти')
+        city_button.bind(on_press=self.find_city_weather)
 
-        bl.add_widget( Label( text = "WEATHER APP", font_size = '20' ) )
-        bl.add_widget( Label( text = "City: {}".format( json['city'] ) ) )
-        bl.add_widget( Label( text = "Temperature: max - {}, min - {}".format( json['max_temp'], json['min_temp'] ) ) )
-        bl.add_widget( Label( text = "Overhead: main - {}, details - {}".format( json['overhead_main'], json['overhead_description'] ) ) )
-        bl.add_widget( Label( text = "Wind speed: {}".format( json['wind_speed'] ) ) )
-        bl.add_widget( Label( text = "Sunrise: {}, Sunset: {}".format( json['sunrise'], json['sunset'] ) ) )
+        bl.add_widget(city_input_label)
+        bl.add_widget(self.city_input)
+        bl.add_widget(city_button)
 
         al.add_widget( bl )
 
-        return al
+        return self.add_widget(al)
 
-    # BUILD APP
+
+    def changer(self, *args):
+        my_screenmanager.current = 'screen2'
+
+
+
+class Weather(Screen):
+    def __init__(self, **kwargs):
+        super ().__init__(**kwargs)
+        self.parse_weather()
+
+    def parse_weather(self):
+
+        # print(my_screenmanager.get_screen('screen1'))
+
+        al = AnchorLayout()
+        bl = BoxLayout( orientation = 'vertical', size_hint = [None, None], size = [300, 200] )
+
+        # bl.add_widget( Label( text = "{}".format( json['name'] ) ) )
+        # bl.add_widget( Label( text = "Температура: макс. - {}, мин. - {}".format( json['main']['temp'], json['main']['temp_min'] ) ) )
+        # bl.add_widget( Label( text = "Погода: main - {}, details - {}".format( json['weather'][0]['main'], json['weather'][0]['description'] ) ) )
+        # bl.add_widget( Label( text = "Скорость ветра: {}".format( json['wind']['speed'] ) ) )
+
+        city_button = Button( text='Назад' )
+        city_button.bind( on_press=self.changer )
+
+        bl.add_widget( city_button )
+
+        al.add_widget( bl )
+
+        return self.add_widget(al)
+
+    def changer(self, *args):
+        my_screenmanager.current = 'screen1'
+
+
+
+
+my_screenmanager = ScreenManager()
+
+
+class WeatherApp(App):
+
     def build(self):
-        return self.parse_weather()
+        my_screenmanager.add_widget( Dashboard( name='screen1' ) )
+        my_screenmanager.add_widget( Weather( name='screen2' ) )
 
-if __name__ == "__main__":
-    # RUN APP
+        return my_screenmanager
+
+
+
+if __name__ == '__main__':
     WeatherApp().run()
+
